f08796a025ab1f92b6f288f53e953ebce22c6fef
Michael <mrathalheim@gmail.com>
2019-11-12 01:39:59 -0600

    Still setting up

---
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+    public void employees(){
+
+    }
+
+    public void initEmployee(String firstName, String lastName, int age, int height, int weight) {
+
+
+
+    }
+
+}
